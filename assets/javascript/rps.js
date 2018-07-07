a6eabcfb38e656ad1712e377a0b001c5ad2c448f
taylornok <taylorowenskees@outlook.com>
2018-07-06 18:43:26 -0700

    not completed

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -28,25 +28,42 @@ $("#start").on("click", function(event) {
   p1Name = $("#userName").val().trim();
 
   database.ref().set({
-      p1name: p1Name
+    p1name: p1Name
   });
 
+  $("player1Name").text("Welcome " + p1name)
   
 
 });
 
+  // If they are connected..
+  if (snap.val()) {
+
+    // Add user to the connections list.
+    var con = connectionsRef.push(true);
+    // Remove user from the connection list when they disconnect.
+    con.onDisconnect().remove();
+  }
+
+
+// When first loaded or when the connections list changes...
+connectionsRef.on("value", function(snap) {
+
+  // Display the viewer count in the html.
+  // The number of online users is the number of children in the connections list.
+  $("#connected-viewers").text(snap.numChildren());
+});
+
 database.ref().on("value", function(snapshot) {
     console.log(snapshot.val());
-    console.log(snapshot.val().name);
+    console.log(snapshot.val().p1name);
 
-    $("#p1Name").text("Welcome " + (snapshot.val().p1Name));
+    // $("#p1Name").text("Welcome " + (snapshot.val().p1Name));
 
 }, function(errorObject) {
     console.log("The read failed: " + errorObject.code);
 
 });
-
-
 
   
 
